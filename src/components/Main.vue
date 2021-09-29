6e0cc6b0b6d517cb41e804c75b362fe5c269496f
Gabrielecorti01 <gcorti.info@gmail.com>
2021-09-29 22:30:03 +0200

    Esercizio non terminato

---
+++ src/components/Main.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="wrapper-container-album">
+        <div class="row">
+            <div v-for="(element, index) in albumList" :key="index" class="col-3 album">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Main',
+
+    data: function(){
+       return {
+           albumList: []
+       }
+    },
+    created: function(){
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((risposta) => {
+            this.albumList = risposta.data.results.slice();
+            console.log(this.albumList)
+        })
+    }
+    
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
